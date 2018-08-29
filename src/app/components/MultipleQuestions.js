792857ccc7954398bbe4739054450ceb2c02fa2b
Rodrigo Villalba <rodris.jvo@gmail.com>
2018-08-29 11:38:29 -0400

    WIP. Add StepZilla library to show one sub-form per family member

--- src/app/components/MultipleQuestions.js
+++ src/app/components/MultipleQuestions.js
@@ -2,30 +2,28 @@ import React, { Component } from 'react';
 import JsonSchemaForm from 'react-jsonschema-form';
 import { getLocalizedSchema } from '../utils/survey_ui_helper';
 
+var StepZilla = require('react-stepzilla').default;
+
 export default class MultipleQuestions extends Component {
   constructor(args) {
     super(args);
-    // console.log(this.props);
+    this.handleSingleFieldClick = this.handleSingleFieldClick.bind(this);
+
     const localizedSchema = getLocalizedSchema(this.props.schema);
+    const stepSchema = this.getStepSchema(localizedSchema);
+
     this.state = {
-      // schema: localizedSchema,
-      // schema for the number of members
       singleSchema: this.getSingleSchema(localizedSchema),
-      // schema for the questions per member
-      // multipleSchema: this.getMultipleSchema(localizedSchema),
-      uiSchema: this.applyConditionsIfAny(this.props.uiSchema)
+
+      stepSchema: this.getStepSchema(localizedSchema),
+      // by default on question per family member
+      steps: [
+        {
+          name: '1',
+          component: <JsonSchemaForm schema={stepSchema} />
+        }
+      ]
     };
-  }
-  applyConditionsIfAny(uiSchema) {
-    // if (this._hasOwnProperty(uiSchema, 'conditions')) {
-    //   const objectWithCond = uiSchema.conditions.keys[0];
-    //   if (this._hasOwnProperty(objectWithCond, 'minItems')) {
-    //     // construct the handle change function
-    //     // according the value of minItems
-    //     // the schema objectWithCond should change?
-    //   }
-    // }
-    return uiSchema;
   }
 
   getSingleSchema(schema) {
@@ -33,27 +31,73 @@ export default class MultipleQuestions extends Component {
     newSchema.properties = Object.keys(schema.properties)
       .filter(key => schema.properties[key].type !== 'array')
       .map(key => schema.properties[key]);
-    return newSchema;
+    // todo improve this
+    return newSchema.properties[0];
   }
 
-  getMultipleSchema(schema) {
+  getStepSchema(schema) {
     const newSchema = Object.assign({}, schema);
     newSchema.properties = Object.keys(schema.properties)
       .filter(key => schema.properties[key].type === 'array')
       .map(key => schema.properties[key]);
-    return newSchema;
+
+    return getLocalizedSchema(newSchema.properties[0].items);
   }
 
   _hasOwnProperty(obj, key) {
     return Object.prototype.hasOwnProperty.call(obj, key);
   }
+
+  handleSingleFieldClick(e) {
+    e.preventDefault();
+    // number of family members
+    const singleFieldNumber = this.singleField.value;
+    if (singleFieldNumber < 2) {
+      return null;
+    }
+    this.setState(() => {
+      const newSteps = [];
+      for (let i = 0; i < singleFieldNumber; i++) {
+        // add one sub-form per family member
+        newSteps.push({
+          name: `${i + 1}`,
+          component: <JsonSchemaForm schema={this.state.stepSchema} />
+        });
+      }
+      return { steps: newSteps };
+    });
+  }
   render() {
+    const { singleSchema, steps } = this.state;
     return (
       <div>
-        <JsonSchemaForm
-          schema={this.state.singleSchema}
-          uiSchema={this.state.uiSchema}
-        />
+        <div className="rdt form-group field field-number">
+          <label className="control-label">{singleSchema.title}</label>
+          <div
+            className="rdt"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <input
+              type="text"
+              className="form-control"
+              ref={input => {
+                this.singleField = input;
+                return input;
+              }}
+              label={`${singleSchema.title}`}
+              placeholder=""
+            />
+            <button onClick={this.handleSingleFieldClick}>Ok</button>
+          </div>
+        </div>
+        <div className="step-progress">
+          <StepZilla steps={steps} />
+        </div>
       </div>
     );
   }
